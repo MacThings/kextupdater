124b313ddb2a3e44f803b4497e78920d7ecddd3b
MacThings <info@sl-soft.de>
2019-08-22 17:58:59 +0200

    .

--- Kext Updater/Website.swift
+++ Kext Updater/Website.swift
@@ -1,31 +1,27 @@
 //
 //  Website.swift
-//  
+//  Kext Updater
 //
-//  Created by Prof. Dr. Luigi on 21.08.19.
+//  Created by Prof. Dr. Luigi on 22.08.19.
+//  Copyright © 2019 Sascha Lamprecht. All rights reserved.
 //
 
 import Cocoa
 import WebKit
 
 class Website: NSViewController {
-
-
+    
     @IBOutlet weak var webView: WebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.preferredContentSize = NSMakeSize(self.view.frame.size.width, self.view.frame.size.height);
-        let url = NSURL (string: "https://update.kextupdater.de/kextupdater/release.html")
+        let urlstring = UserDefaults.standard.string(forKey: "SourceURL")
+        let url = NSURL (string: urlstring!)
         let requestObj = NSURLRequest(url: url! as URL)
+        webView.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 9_3 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13E188a Safari/601.1"
         webView.mainFrame.load(requestObj as URLRequest)
-        
     }
-    
-    override var representedObject: Any? {
-        didSet {
-            // Update the view, if already loaded.
-        }
-    }
-    
 }
+
+
