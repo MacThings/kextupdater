52422291c82cdd7e9b859a9fe53f9a75353566f7
MacThings <info@sl-soft.de>
2019-08-22 18:04:26 +0200

    .

---
+++ Kext Updater/Changelog.swift
@@ -0,0 +1,31 @@
+//
+//  Website.swift
+//  
+//
+//  Created by Prof. Dr. Luigi on 21.08.19.
+//
+
+import Cocoa
+import WebKit
+
+class Changelog: NSViewController {
+
+
+    @IBOutlet weak var webView: WebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.preferredContentSize = NSMakeSize(self.view.frame.size.width, self.view.frame.size.height);
+        let url = NSURL (string: "https://update.kextupdater.de/kextupdater/release.html")
+        let requestObj = NSURLRequest(url: url! as URL)
+        webView.mainFrame.load(requestObj as URLRequest)
+        
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+}
---
+++ Kext Updater/Website.swift
@@ -0,0 +1,27 @@
+//
+//  Website.swift
+//  Kext Updater
+//
+//  Created by Prof. Dr. Luigi on 22.08.19.
+//  Copyright © 2019 Sascha Lamprecht. All rights reserved.
+//
+
+import Cocoa
+import WebKit
+
+class Website: NSViewController {
+
+    @IBOutlet weak var webView: WebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.preferredContentSize = NSMakeSize(self.view.frame.size.width, self.view.frame.size.height);
+        let urlstring = UserDefaults.standard.string(forKey: "SourceURL")
+        let url = NSURL (string: urlstring!)
+        let requestObj = NSURLRequest(url: url! as URL)
+        webView.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 9_3 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13E188a Safari/601.1"
+        webView.mainFrame.load(requestObj as URLRequest)
+    }
+}
+
+
