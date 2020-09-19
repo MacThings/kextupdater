d114d47e7d87be32e59dca079d4477f8548eadc6
MacThings <info@sl-soft.de>
2020-09-19 09:01:40 +0200

    Forgot something

---
+++ Kext Updater/BigSurPlus.swift
@@ -0,0 +1,32 @@
+//
+//  BigSurPlus.swift
+//  Kext Updater
+//
+//  Created by Sascha Lamprecht on 16.08.19.
+//  Copyright © 2019 Sascha Lamprecht. All rights reserved.
+//
+
+import Cocoa
+
+class BigSurPlus: NSViewController {
+    
+    @IBOutlet weak var close_window: NSButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        self.preferredContentSize = NSMakeSize(self.view.frame.size.width, self.view.frame.size.height);
+    }
+
+    @IBAction func close_window(_ sender: Any) {
+        self.view.window?.close()
+    }
+    
+    @objc func cancel(_ sender: Any?) {
+        self.view.window?.close()
+    }
+
+}
+
+
+
