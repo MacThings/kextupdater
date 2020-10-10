19e198a34e7668b261262bc04bfdb3d0c45932f4
MacThings <info@sl-soft.de>
2020-10-10 20:08:58 +0200

    Fixed itlwm stuff and added intelmausi nightly

---
+++ Kext Updater/About.swift
@@ -0,0 +1,38 @@
+//
+//  About.swift
+//  Kext Updater
+//
+//  Created by Sascha Lamprecht on 16.08.19.
+//  Copyright © 2019 Sascha Lamprecht. All rights reserved.
+//
+
+import Cocoa
+
+class About: NSViewController {
+    
+    @IBOutlet weak var close_window: NSButton!
+    @IBOutlet weak var copyright: NSTextField!
+    
+    let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy"
+        let dateStr = formatter.string(from: NSDate() as Date)
+        self.copyright.stringValue = "© " + dateStr + " Sascha Lamprecht"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+
+    @objc func cancel(_ sender: Any?) {
+        self.view.window?.close()
+    }
+
+}
+
+
+
